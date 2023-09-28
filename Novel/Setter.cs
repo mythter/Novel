031ab7bc7ac533cb2fc9c67972b74bac9087cace
Anton <anton260903@gmail.com>
2023-09-28 18:56:34 +0300

    partial classes added

---
+++ Novel/Setter.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novel
+{
+    public partial class Form1
+    {
+        Bitmap? characterIcon;
+        Bitmap? leftcharacterImage;
+        Bitmap? rightcharacterImage;
+        Bitmap chronoIcon = Properties.Resources.Crono_icon_Round;
+
+        private void SetSideCharacters()
+        {
+            Random rand = new Random();
+            rightPictureBox.Visible = true;
+            leftPictureBox.Visible = true;
+            int choice = rand.Next(2);
+
+            leftPictureBox.Image = choice == 0 ? Properties.Resources.Crono_left : leftcharacterImage;
+            rightPictureBox.Image = choice == 0 ? rightcharacterImage : Properties.Resources.Crono_right;
+        }
+
+        private void SetCharacterImages()
+        {
+            switch (character)
+            {
+                case "Magus":
+                    characterIcon = Properties.Resources.Magus_icon_Round;
+                    leftcharacterImage = Properties.Resources.Magus_left;
+                    rightcharacterImage = Properties.Resources.Magus_right;
+                    break;
+                case "Frog":
+                    characterIcon = Properties.Resources.Frog_icon_Round;
+                    leftcharacterImage = Properties.Resources.Frog_left;
+                    rightcharacterImage = Properties.Resources.Frog_right;
+                    break;
+                case "Lucca":
+                    characterIcon = Properties.Resources.Lucca_icon_Round;
+                    leftcharacterImage = Properties.Resources.Lucca_left;
+                    rightcharacterImage = Properties.Resources.Lucca_right;
+                    break;
+                case "Marle":
+                    characterIcon = Properties.Resources.Marle_icon_Round;
+                    leftcharacterImage = Properties.Resources.Marle_left;
+                    rightcharacterImage = Properties.Resources.Marle_right;
+                    break;
+                case "Robo":
+                    characterIcon = Properties.Resources.Robo_icon_Round;
+                    leftcharacterImage = Properties.Resources.Robo_left;
+                    rightcharacterImage = Properties.Resources.Robo_right;
+                    break;
+                case "Ayla":
+                    characterIcon = Properties.Resources.Ayla_icon_Round;
+                    leftcharacterImage = Properties.Resources.Ayla_left;
+                    rightcharacterImage = Properties.Resources.Ayla_right;
+                    break;
+            }
+        }
+
+        private void SetTransperency()
+        {
+            Point pos;
+
+            if (startLabel?.Parent is not null)
+            {
+                pos = startLabel.Parent.PointToScreen(startLabel.Location);
+                pos = backPictureBox.PointToClient(pos);
+                startLabel.Parent = backPictureBox;
+                startLabel.Location = pos;
+                startLabel.BackColor = Color.Transparent;
+            }
+
+            if (bottomPanel?.Parent is not null)
+            {
+                pos = bottomPanel.Parent.PointToScreen(bottomPanel.Location);
+                pos = backPictureBox.PointToClient(pos);
+                bottomPanel.Parent = backPictureBox;
+                bottomPanel.Location = pos;
+                bottomPanel.BackColor = Color.Transparent;
+            }
+
+            if (characterTextLabel?.Parent is not null)
+            {
+                pos = characterTextLabel.Parent.PointToScreen(characterTextLabel.Location);
+                pos = backgroudTextPicBox.PointToClient(pos);
+                characterTextLabel.Parent = backgroudTextPicBox;
+                characterTextLabel.Location = pos;
+                characterTextLabel.BackColor = Color.Transparent;
+            }
+
+            if (characterIconPicBox?.Parent is not null)
+            {
+                pos = characterIconPicBox.Parent.PointToScreen(characterIconPicBox.Location);
+                pos = backgroudTextPicBox.PointToClient(pos);
+                characterIconPicBox.Parent = backgroudTextPicBox;
+                characterIconPicBox.Location = pos;
+                characterIconPicBox.BackColor = Color.Transparent;
+            }
+
+            if (leftPictureBox?.Parent is not null)
+            {
+                pos = leftPictureBox.Parent.PointToScreen(leftPictureBox.Location);
+                pos = backPictureBox.PointToClient(pos);
+                leftPictureBox.Parent = backPictureBox;
+                leftPictureBox.Location = pos;
+                leftPictureBox.BackColor = Color.Transparent;
+            }
+
+            if (rightPictureBox?.Parent is not null)
+            {
+                pos = rightPictureBox.Parent.PointToScreen(rightPictureBox.Location);
+                pos = backPictureBox.PointToClient(pos);
+                rightPictureBox.Parent = backPictureBox;
+                rightPictureBox.Location = pos;
+                rightPictureBox.BackColor = Color.Transparent;
+            }
+
+            if (frogPointer?.Parent is not null)
+            {
+                pos = frogPointer.Parent.PointToScreen(frogPointer.Location);
+                pos = backPictureBox.PointToClient(pos);
+                frogPointer.Parent = backPictureBox;
+                frogPointer.Location = pos;
+                frogPointer.BackColor = Color.Transparent;
+            }
+
+            if (aylaPointer?.Parent is not null)
+            {
+                pos = aylaPointer.Parent.PointToScreen(aylaPointer.Location);
+                pos = backPictureBox.PointToClient(pos);
+                aylaPointer.Parent = backPictureBox;
+                aylaPointer.Location = pos;
+                aylaPointer.BackColor = Color.Transparent;
+            }
+
+            if (magusPointer?.Parent is not null)
+            {
+                pos = magusPointer.Parent.PointToScreen(magusPointer.Location);
+                pos = backPictureBox.PointToClient(pos);
+                magusPointer.Parent = backPictureBox;
+                magusPointer.Location = pos;
+                magusPointer.BackColor = Color.Transparent;
+            }
+
+            if (marlePointer?.Parent is not null)
+            {
+                pos = marlePointer.Parent.PointToScreen(marlePointer.Location);
+                pos = backPictureBox.PointToClient(pos);
+                marlePointer.Parent = backPictureBox;
+                marlePointer.Location = pos;
+                marlePointer.BackColor = Color.Transparent;
+            }
+
+            if (luccaPointer?.Parent is not null)
+            {
+                pos = luccaPointer.Parent.PointToScreen(luccaPointer.Location);
+                pos = backPictureBox.PointToClient(pos);
+                luccaPointer.Parent = backPictureBox;
+                luccaPointer.Location = pos;
+                luccaPointer.BackColor = Color.Transparent;
+            }
+
+            if (roboPointer?.Parent is not null)
+            {
+                pos = roboPointer.Parent.PointToScreen(roboPointer.Location);
+                pos = backPictureBox.PointToClient(pos);
+                roboPointer.Parent = backPictureBox;
+                roboPointer.Location = pos;
+                roboPointer.BackColor = Color.Transparent;
+            }
+        }
+
+        private void SetStartScreen()
+        {
+            rightPictureBox.Visible = false;
+            leftPictureBox.Visible = false;
+            bottomPanel.Visible = false;
+            backPictureBox.Image = Properties.Resources.Title_Screen_Normal;
+            startLabel.Visible = true;
+        }
+    }
+}
---
+++ Novel/Story.cs
@@ -0,0 +1,147 @@
+﻿namespace Novel
+{
+    public partial class Form1
+    {
+        int story = 0;
+        string? character;
+
+        bool animating = false;
+
+        string[] cronoReply = Properties.Resources.CronoReply.Split('\n');
+        string[]? characterReply;
+        string[]? cronoCharacterReply;
+
+        private async Task StoryStep()
+        {
+            if (animating)
+                return;
+
+            if (story == 0 ||
+                story == 5 ||
+                story == 7 ||
+                story == 9)
+            {
+                await PrintCronoReply();
+                story++;
+            }
+            else if (story == 1)
+            {
+                bool on = true;
+                frogPointer.Visible = on;
+                roboPointer.Visible = on;
+                marlePointer.Visible = on;
+                magusPointer.Visible = on;
+                aylaPointer.Visible = on;
+                luccaPointer.Visible = on;
+                characterIconPicBox.Image = null;
+                animating = true;
+                await Print("Choose character", characterTextLabel);
+                animating = false;
+                story++;
+            }
+            else if (story == 2)
+            {
+                bool off = false;
+                frogPointer.Visible = off;
+                roboPointer.Visible = off;
+                marlePointer.Visible = off;
+                magusPointer.Visible = off;
+                aylaPointer.Visible = off;
+                luccaPointer.Visible = off;
+                SetCharacterImages();
+                characterIconPicBox.Image = characterIcon;
+                await PrintCharacterReply();
+                story++;
+            }
+            else if (story == 3)
+            {
+                backPictureBox.Image = Properties.Resources.Scene_2_Normal;
+                SetSideCharacters();
+                characterIconPicBox.Image = chronoIcon;
+                await PrintCronoReply();
+                story++;
+            }
+            else if (story == 4 ||
+                     story == 6 ||
+                     story == 8 ||
+                     story == 10)
+            {
+                await PrintCharacterReply();
+                story++;
+            }
+            else if (story == 11)
+            {
+                story = 0;
+                SetStartScreen();
+            }
+        }
+
+        private async Task PrintCharacterReply()
+        {
+            characterIconPicBox.Image = characterIcon;
+            animating = true;
+            if (story == 2)
+            {
+                await Print(characterReply?[0], characterTextLabel);
+            }
+            else if (story == 4)
+            {
+                await Print(characterReply?[1], characterTextLabel);
+            }
+            else if (story == 6)
+            {
+                await Print(characterReply?[2], characterTextLabel);
+            }
+            else if (story == 8)
+            {
+                await Print(characterReply?[3], characterTextLabel);
+            }
+            else if (story == 10)
+            {
+                await Print(characterReply?[4], characterTextLabel);
+            }
+            animating = false;
+        }
+
+        private async Task PrintCronoReply()
+        {
+            characterIconPicBox.Image = chronoIcon;
+            animating = true;
+            if (story == 0)
+            {
+                await Print(cronoReply[0], characterTextLabel);
+            }
+            else if (story == 3)
+            {
+                await Print(cronoCharacterReply?[0], characterTextLabel);
+
+            }
+            else if (story == 5)
+            {
+                await Print(cronoCharacterReply?[1], characterTextLabel);
+            }
+            else if (story == 7)
+            {
+                await Print(cronoCharacterReply?[2], characterTextLabel);
+            }
+            else if (story == 9)
+            {
+                await Print(cronoCharacterReply?[3], characterTextLabel);
+            }
+            animating = false;
+        }
+
+        private async Task Print(string? text, Label output)
+        {
+            if (output is null || text is null)
+                return;
+
+            output.Text = "";
+            foreach (var ch in text)
+            {
+                output.Text += ch;
+                await Task.Delay(50);
+            }
+        }
+    }
+}
